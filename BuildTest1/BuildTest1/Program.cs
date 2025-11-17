f101aca9eacc33bd771f49779490b09b028c85cf
DiverKonuhov <leva3480@gmail.com>
2025-11-17 13:17:30 +0300

    Add project files and content

---
+++ BuildTest1/BuildTest1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+class Program
+{
+    public void ViewTextFile()
+    {
+        int testy;
+        testy = 0;
+
+        string TextFileNumber1 = @"C:\Users\Perephiriya\source\repos\BuildTest1\BuildTest1\InvertReader.txt";
+        string text = File.ReadAllText(TextFileNumber1);
+
+        Console.WriteLine(text);
+        return;
+    }
+    static public void Main()
+    {
+        Program obj = new Program();
+        obj.ViewTextFile();
+    }
+}
